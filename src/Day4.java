e5310426f8d3ea1d50b68b4474459df87b4f3c94
Michael Kearns <mobiuscog@users.noreply.github.com>
2021-12-04 13:21:42 +0000

    After coffee, thought of a simpler Board structure

--- src/Day4.java
+++ src/Day4.java
@@ -1,128 +1,100 @@
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Optional;
 
 public class Day4
 {
+  private static class BingoSquare
+  {
+    final int row;
+    final int column;
+    boolean isMarked = false;
 
-  private static class Board {
-    private final List<List<Integer>> rows = new ArrayList<>(5);
-    private final List<List<Integer>> columns = new ArrayList<>(5);
+    BingoSquare(int row, int column) {
+      this.row = row;
+      this.column = column;
+    }
+  }
 
-    Board(List<String> boardLines) {
-      for (int count=0; count<5; count++) {
-        columns.add(new ArrayList<>(5));
-      }
-      for (String boardLine : boardLines) {
-        List<Integer> numbers = new ArrayList<>(Arrays.stream(boardLine.trim().split("\\s+")).map(Integer::parseInt).toList());
-        rows.add(numbers);
-        for (int columnOffset = 0; columnOffset < numbers.size(); columnOffset++) {
-          List<Integer> columnNumbers = columns.get(columnOffset);
-          columnNumbers.add(numbers.get(columnOffset));
+  private static class BingoBoard {
+
+    private final HashMap<Integer, BingoSquare> bingoSquares = new HashMap<>();
+
+    private final int[] rows;
+    private final int[] columns;
+    private boolean hasWon;
+
+    BingoBoard(List<String> boardLines) {
+      rows = new int[boardLines.size()];
+      columns = new int[boardLines.get(0).length()];
+      hasWon = false;
+      for (int row=0; row<boardLines.size(); row++) {
+        var numbers = Arrays.stream(boardLines.get(row).trim().split("\\s+"))
+            .map(Integer::parseInt).toList();
+        for (int column=0; column<numbers.size(); column++) {
+          bingoSquares.put(numbers.get(column), new BingoSquare(row, column));
+          rows[row]++;
+          columns[column]++;
         }
       }
     }
 
     Optional<Integer> numberSelected(int number) {
-      Optional<Integer> result = checkList(rows, number);
-      boolean matched = false;
-      Integer value = 0;
-      if (result.isPresent()) {
-        value = result.get();
-        matched = true;
+      var square = bingoSquares.get(number);
+      if (square != null && !square.isMarked) {
+        square.isMarked = true;
+        if (--rows[square.row] == 0 || --columns[square.column] == 0) {
+          hasWon = true;
+          return Optional.of(bingoSquares.entrySet().stream()
+              .filter(e -> !e.getValue().isMarked)
+              .map(Entry::getKey)
+              .mapToInt(Integer::intValue).sum());
+        }
       }
-      result = checkList(columns, number);
-      if (result.isPresent()) {
-        value = result.get();
-        matched = true;
-      }
-      return matched ? Optional.of(value) : Optional.empty();
+      return Optional.empty();
     }
 
-    Optional<Integer> checkList(List<List<Integer>> list, int number) {
-      boolean matched = false;
-      for (List<Integer> listEntry: list) {
-        if (listEntry.remove((Integer)number)) {
-          if (listEntry.size()==0) {
-            matched = true;
-            break;
-          }
-        }
-      }
-      if (matched) {
-        int total = 0;
-        for (List<Integer> listEntry: list) {
-          total += listEntry.stream().mapToInt(Integer::intValue).sum();
-        }
-        return Optional.of(total);
-      } else {
-        return Optional.empty();
-      }
+    public boolean hasWon() {
+      return hasWon;
     }
   }
 
   public static void main(String[] args) {
     var lines = Util.readFile(Path.of("resources/day4.dat"));
-    var lineCount = lines.size();
-
-    // Part 1
-    List<Integer> numbers = Arrays.stream(lines.get(0).split(",")).map(Integer::parseInt).toList();
+    var numbers = Arrays.stream(lines.get(0).split(",")).map(Integer::parseInt).toList();
 
     // Build boards
     int lineOffset = 2;
-    List<Board> boards = new ArrayList<>((lines.size()-2) / 6);
+    var boards = new ArrayList<BingoBoard>((lines.size()-2) / 6);
     while (lineOffset < lines.size()) {
-      boards.add(new Board(lines.subList(lineOffset, lineOffset+5)));
+      boards.add(new BingoBoard(lines.subList(lineOffset, lineOffset+5)));
       lineOffset += 6;
     }
 
-    boolean foundBingo = false;
+    record Win(int number, int boardRemaining) {}
+
+    // Record wins
+    var wins = new ArrayList<Win>();
     for (Integer number: numbers) {
-      for (Board board: boards) {
-        Optional<Integer> bingo = board.numberSelected(number);
-        if (bingo.isPresent()) {
-          System.out.printf("Bingo !! -> %d%n", bingo.get() * number);
-          foundBingo = true;
-          break;
+      for (BingoBoard board: boards) {
+        if (!board.hasWon()) {
+          Optional<Integer> bingo = board.numberSelected(number);
+          bingo.ifPresent(integer -> wins.add(new Win(number, integer)));
         }
       }
-      if (foundBingo) {
-        break;
-      }
     }
+
+    // Part 1
+    Win firstWin = wins.get(0);
+    System.out.printf("Bingo !! -> %d%n", firstWin.number() * firstWin.boardRemaining());
 
     // Part 2
-    lineOffset = 2;
-    boards = new ArrayList<>((lines.size()-2) / 6);
-    while (lineOffset < lines.size()) {
-      boards.add(new Board(lines.subList(lineOffset, lineOffset+5)));
-      lineOffset += 6;
-    }
-
-    int lastBingo = 0;
-    int lastNumber = 0;
-    List<Board> winningBoards = new ArrayList<>();
-    for (Integer number: numbers) {
-      for (Board board: boards) {
-        Optional<Integer> bingo = board.numberSelected(number);
-        if (bingo.isPresent()) {
-          lastBingo = bingo.get();
-          lastNumber = number;
-          winningBoards.add(board);
-        }
-      }
-      if (winningBoards.size()>0) {
-        boards.removeAll(winningBoards);
-        winningBoards.clear();
-      }
-      if (boards.size()==0) {
-        break;
-      }
-    }
-    System.out.printf("Squid Bingo !! -> %d%n", lastBingo * lastNumber);
-
+    Win lastWin = wins.get(wins.size()-1);
+    System.out.printf("Squid Bingo !! -> %d%n", lastWin.number() * lastWin.boardRemaining());
   }
 }
 
